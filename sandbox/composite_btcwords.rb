5d0f6b7c74fb15a6bc6fbb8fedb51180779a9b90
Gerald Bauer <gerald.bauer@gmail.com>
2023-06-08 08:58:46 +0200

    up

--- sandbox/composite_btcwords.rb
+++ sandbox/composite_btcwords.rb
@@ -15,12 +15,10 @@ recs = read_csv( "./#{slug}/mint.csv" )
 puts "   #{recs.size} record(s)"
 
 
-## cut down to first hundred
-# offset = 100
-# recs = recs[offset, 100]
 
-## cut down to first fifty
-recs = recs[0, 50]
+## cut down to first fifty, next fifty
+offset = 150
+recs = recs[offset, 50]
 puts "   #{recs.size} record(s)"
 
 
@@ -31,31 +29,26 @@ spritesheet = ImageComposite.read( spritesheet_path,
                                       width: width,
                                       height: height )
 
-puts "  #{spritesheet.width}x#{spritesheet.height}"
-puts "  #{spritesheet.tile_width}x#{spritesheet.tile_height}"
-puts "  #{spritesheet.count}"
-
-
-
 composite = ImageComposite.new( 5, 10,  width: width,
                                         height: height )
+
 
 recs.each_with_index do |rec,i|
   img = Image.new( width, height )
   g = rec['g'].split( ' ' ).map {|v| v.to_i(10) }
-  puts "==> no. #{i}   g: #{rec['g']} - #{g.inspect}"
+  puts "==> no. #{i+offset}   g: #{rec['g']} - #{g.inspect}"
   g.each do |num|
      img.compose!( spritesheet[ num ] )
   end
 
-  img.save( "./tmp/#{slug}#{i}.png" )
-  img.zoom(4).save( "./tmp/#{slug}#{i}@4x.png" )
+  img.save( "./tmp/#{slug}#{i+offset}.png" )
+  img.zoom(4).save( "./tmp/#{slug}#{i+offset}@4x.png" )
 
   composite << img
 end
 
 
-outname = "./tmp/#{slug}"
+outname = "./tmp/#{slug}_#{offset}"
 
 composite.save( "#{outname}.png" )
 composite.zoom(4).save( "#{outname}@4x.png" )
