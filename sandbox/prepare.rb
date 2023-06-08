4a443d3e37cd2136b42ab84453893936c190f624
Gerald Bauer <gerald.bauer@gmail.com>
2023-06-08 16:33:29 +0200

    up

--- sandbox/prepare.rb
+++ sandbox/prepare.rb
@@ -80,7 +80,7 @@ pages = {
 #  'diypunks-v2' => [ 'iipunk', 5 ],
 #  'diyordibots' => [ 'bot', 44 ],
 #   'diycoolcats' => [ 'cat', 27 ],
-    'btcwords' => [ 'word/',11 ],
+    'btcwords' => [ 'word/',12 ],
 #  'diyordibots-v2' => [ 'iibot/', 40 ],
 
 }
