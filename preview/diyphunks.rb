f277e2f214602f573be5ec745d1d4fc4f2930e30
Gerald Bauer <gerald.bauer@gmail.com>
2023-06-25 00:54:15 +0200

    up

---
+++ preview/diyphunks.rb
@@ -0,0 +1,32 @@
+####
+#  to run use
+#    $ ruby preview/diyphunks.rb
+
+require 'ordgen'
+
+
+## step 1 - read (local) spritesheet.png ("art layers")
+diyphunks   = Ordgen.read( './diyphunks/spritesheet.png',
+                                          width: 24,
+                                          height: 24 )
+
+
+
+specs = [
+  [5,57,38],
+  [4,57,38],
+]
+
+
+specs.each do |attributes|
+  puts "==> phunk #{attributes.inspect}"
+  punk = diyphunks.generate( *attributes )
+
+  path = "./preview/tmp/phunk-#{attributes.join('_')}"
+  punk.save( path+'.png' )
+  punk.zoom(4).save( path+'@4x.png' )
+end
+
+
+
+puts "bye"
